49846c6d8dc2bb412ef96e75ee6084bc22381b4e
bglddavies <birkystomper99@gmail.com>
2023-07-18 18:53:01 +0100

    patching directory mount issue in generate key and made email optional

--- ssh-keygen/generate-key.sh
+++ ssh-keygen/generate-key.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -eou pipefail
 
-DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 cd "$DIR"
 
-: "${GITHUB_EMAIL}:?"GITHUB_EMAIL must be set. Aborting."}"
+: "${EMAIL:=""}"
 : "${OUTPUT_FILE_NAME:="id_rsa"}"
 
 : "${IMAGE_NAME:="alpinesshkeygen"}"
 : "${IMAGE_VERSION:="latest"}"
 
-docker run --rm -v certificates:/certificates "$IMAGE_NAME:$IMAGE_VERSION" /bin/sh -c "ssh-keygen -t rsa -b 4096 -C \"$GITHUB_EMAIL\" -f \"/certificates/${OUTPUT_FILE_NAME}\""
+CTAG=""
+if [ ! -z "${EMAIL}" ]; then
+    CTAG="-C \"${EMAIL}\""
+fi
+
+docker run --rm -v "${DIR}/certificates:/certificates" "$IMAGE_NAME:$IMAGE_VERSION" /bin/sh -c "ssh-keygen ${CTAG} -t rsa -b 4096 -f \"/certificates/${OUTPUT_FILE_NAME}\""
